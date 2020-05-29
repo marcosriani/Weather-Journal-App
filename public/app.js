2716af2b2dd60fe7be8cd16a5777513e06362041
Marcos Riani <marcosriani@hotmail.com>
2020-05-29 21:15:48 +0100

    First commit

---
+++ public/app.js
@@ -0,0 +1,126 @@
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = `${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`;
+
+// Getting Form information
+const submitButton = document.querySelector('#generate');
+const zip = document.querySelector('#zip');
+const textArea = document.querySelector('#feelings');
+
+//   Areas of the UI to be updated
+const dateArea = document.querySelector('#date');
+const tempArea = document.querySelector('#temp');
+const contentArea = document.querySelector('#content');
+
+// Container where the data will be updated on the UI
+const containerDiv = document.querySelector('#entryHolder');
+
+// WEATHER API
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?';
+const countryCode = 'US';
+const apiKey = '303443fb1055dd49d261c723b8c45327';
+
+zip.addEventListener('change', (event) => {
+  if (event.target.value.length < 5) {
+    alert('Zip Code incomplete');
+  }
+  if (isNaN(event.target.value)) {
+    alert('Zip Code is a number');
+  }
+});
+
+submitButton.addEventListener('click', (event) => {
+  const textAreaContent = textArea.value;
+  const zipCode = zip.value;
+
+  event.preventDefault();
+
+  containerDiv.innerHTML = '';
+  // Get request
+  const getWeather = async (baseURL, zipCode, countryCode, apiKey) => {
+    const urlToFetch = `${baseURL}zip=${zipCode},${countryCode}&units=metric&appid=${apiKey}`;
+    const response = await fetch(urlToFetch);
+    try {
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log('Error', error);
+    }
+  };
+
+  // Post request
+  const postData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    try {
+      const newData = await response.json();
+      console.log(newData);
+      return newData;
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  const updateUI = async () => {
+    const request = await fetch('/all');
+    try {
+      const returnedData = await request.json();
+
+      const createWrapperElement = (title, elementClass) => {
+        //   Wrapper div
+        const divElement = document.createElement('div');
+        const spanElement = document.createElement('span');
+
+        // Adding class
+        divElement.classList.add(elementClass);
+
+        // Appending elements
+        containerDiv.appendChild(divElement);
+        divElement.innerHTML = title;
+        divElement.appendChild(spanElement);
+
+        return spanElement;
+      };
+
+      returnedData.forEach((dataItem) => {
+        // Creating elements and appending content
+        if (dataItem.temperature !== undefined) {
+          createWrapperElement('Temperature: ', 'temperature').innerHTML =
+            dataItem.temperature;
+        }
+        if (dataItem.currentTime !== undefined) {
+          createWrapperElement('Date: ', 'date').innerHTML =
+            dataItem.currentTime;
+        }
+        if (dataItem.userResponse !== undefined) {
+          createWrapperElement('Feeling: ', 'content').innerHTML =
+            dataItem.userResponse;
+        }
+      });
+
+      //   reset form
+      zip.value = '';
+      textArea.value = '';
+    } catch (error) {
+      console.log('Update UI error', error);
+    }
+  };
+
+  //   Chaining of Promises
+  getWeather(baseURL, zipCode, countryCode, apiKey)
+    .then((data) => {
+      data.main.currentTime = newDate;
+      data.main.userResponse = textAreaContent;
+      postData('/database', data);
+    })
+    .then(() => {
+      updateUI();
+    });
+});
---
+++ server.js
@@ -0,0 +1,46 @@
+// Require Express to run server and routes
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+// Setup empty JS object to act as endpoint for all routes
+const projectData = [];
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('public'));
+
+// Routers
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + 'index.html');
+  res.send(projectData);
+});
+
+app.post('/database', (req, res) => {
+  const { temp, currentTime, userResponse } = req.body.main;
+  projectData.push({ temperature: `${temp}°C` });
+  projectData.push({ currentTime: currentTime });
+  projectData.push({ userResponse: userResponse });
+  res.send(projectData);
+});
+
+app.get('/all', (req, res) => {
+  res.send(projectData);
+});
+
+// Setup Server
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`'App running on port ${port}`);
+});
